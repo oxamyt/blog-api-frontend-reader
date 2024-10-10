dc3b612c08acb3013daa9e309a70e32ae9a67f45
Oxamyt <dentez.danil@gmail.com>
2024-10-10 19:48:57 +0300

    create login component

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+
 function LoginPage() {
+  const [responseData, setResponseData] = useState(null);
+  const [error, setError] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    event.preventDefault();
+
+    const data = { username, password };
+
+    try {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      console.log(response);
+
+      if (!response.ok) {
+        throw new Error("Failed to login. Please try again.");
+      }
+
+      const responseData = await response.json();
+      setResponseData(responseData);
+      setError(null);
+      setUsername("");
+      setPassword("");
+
+      localStorage.setItem("token", responseData.token);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   return (
     <div>
-      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      {responseData && (
+        <div>
+          <h2>Response Data:</h2>
+          <pre>{JSON.stringify(responseData, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 }
